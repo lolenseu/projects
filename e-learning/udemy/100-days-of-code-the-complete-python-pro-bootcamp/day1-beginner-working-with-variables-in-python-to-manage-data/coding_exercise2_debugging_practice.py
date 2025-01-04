e8690265fa227cb8186b107f27f1d997eb061cd0
lolenseu <lolenseu.koleutejeu248@gmail.com>
2025-01-04 12:38:09 +0800

    add day1 of 100 day bootcamp in python

---
+++ e-learning/udemy/100-days-of-code-the-complete-python-pro-bootcamp/day1-beginner-working-with-variables-in-python-to-manage-data/coding_exercise2_debugging_practice.py
@@ -0,0 +1,5 @@
+print("Notes from Day 1")
+print("The print statement is used to output strings")
+print("Strings are strings of characters")
+print("String Concatenation is done with the + sign")
+print("New lines can be created with a \ and the letter n")
---
+++ e-learning/udemy/100-days-of-code-the-complete-python-pro-bootcamp/day1-beginner-working-with-variables-in-python-to-manage-data/coding_exercise3_variables.py
@@ -0,0 +1,6 @@
+glass1 = "milk"
+glass2 = "juice"
+
+glass1, glass2 = glass2, glass1
+print("Glass1 is now:", glass1)
+print("Glass2 is now:", glass2)
