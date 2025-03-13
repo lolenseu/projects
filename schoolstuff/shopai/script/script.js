f24a980ccfb7d7de203166574bcbee46df148360
lolenseu <lolenseu.koleutejeu248@gmail.com>
2025-03-14 00:18:52 +0800

    up

---
+++ schoolstuff/shopai/script/script.js
@@ -0,0 +1,36 @@
+let slideIndex = 0;
+let slideInterval;
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1}    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    slideInterval = setTimeout(showSlides, 2000);
+}
+
+function currentSlide(n) {
+    clearTimeout(slideInterval);
+    slideIndex = n;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";  
+    dots[slideIndex-1].className += " active";
+    slideInterval = setTimeout(showSlides, 5000);
+}
