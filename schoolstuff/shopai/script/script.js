a2552790a618215f6aac045e4f3b0ed5e70b77e0
lolenseu <lolenseu.koleutejeu248@gmail.com>
2025-03-14 14:32:16 +0800

    up

--- schoolstuff/shopai/script/script.js
+++ schoolstuff/shopai/script/script.js
@@ -1,3 +1,5 @@
+
+// Slideshow and Dots
 let slideIndex = 0;
 let slideInterval;
 showSlides();
@@ -33,4 +35,20 @@ function currentSlide(n) {
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
     slideInterval = setTimeout(showSlides, 5000);
+}
+
+
+// Backtotop Button
+window.onscroll = function() {
+    var backToTopButton = document.getElementById("back-to-top");
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
+
+function scrollToTop() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
 }
